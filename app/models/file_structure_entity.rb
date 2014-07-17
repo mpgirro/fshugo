dfa36b4263d553fc4ab03001a4a26c18a4a73be7
Maximilian Irro <max@disposia.org>
2014-07-17 17:54:51 +0200

    added new accessible attributes

--- app/models/file_structure_entity.rb
+++ app/models/file_structure_entity.rb
@@ -1,3 +1,5 @@
 class FileStructureEntity < ActiveRecord::Base
   attr_accessible :path, :bytes, :ctime, :mtime, :type
+  attr_accessible :file_count, :item_count # used if referencing a directory
+  attr_accessible :mime_id, :kind_id # used if referencing a file
 end
