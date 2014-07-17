b77cde52ac706044f9faac27da3a36b7b526b27b
Maximilian Irro <max@disposia.org>
2014-07-17 19:38:03 +0200

    wrote the search method

--- app/controllers/hugo_controller.rb
+++ app/controllers/hugo_controller.rb
@@ -1,4 +1,40 @@
 class HugoController < ApplicationController
   def search
+    @query = params[:search_query]
+    parts = @query.split(" ")
+    
+    # extract conditions, make fit for sql usage
+    conditions = []
+    parts.each do |part|
+      # substitute wildcard symbols
+      part["%"] = "\%"
+      part["_"] = "\_"
+      part =  "%" + part + "%"
+      conditions << part
+    end
+    
+    # make conditions string
+    condition_base = "path LIKE ? AND "
+    condition_string = ""
+    conditions.each do |condition|
+      condition_string += condition_base
+    end
+    condition_string = condition_string[0, condition_string.length - "AND ".length]
+    
+    # make conditions array
+    cond = []
+    cond << condition_string
+    conditions.each do |c|
+      cond << c
+    end
+    
+    # do the search
+    @results = FileStructureEntity.find(:all, :conditions => cond)
+  
+    # do the rating
+    
+  
+    # render result page
+    render "results"
   end
 end
