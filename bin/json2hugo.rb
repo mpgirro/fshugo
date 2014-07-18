72226e0aeb69bbcad0328443c68102df6487f537
Maximilian Irro <max@disposia.org>
2014-07-18 14:10:35 +0200

    this makes json inventory import way faster

--- bin/json2hugo.rb
+++ bin/json2hugo.rb
@@ -3,6 +3,16 @@
 require 'rubygems'
 require 'json'
 
+module JSON
+  class << self
+    def parse(source, opts = {})
+      opts = ({:max_nesting => 100}).merge(opts)
+      Parser.new(source, opts).parse
+    end
+  end
+end
+
+
 def parse_fstruct(fstruct)
   
   fstruct.each do |entry|
@@ -48,39 +58,49 @@ end
 
 invfile_path = ARGV[0]
 
+puts "reading #{invfile_path}"
 f = File.open(invfile_path, "r")
-
 json_string = ""
-f.each_line do |line|
-  json_string += line
-end
+
+# read all lines into one large array
+# is fast, but takes way more memory
+json_string = f.readlines.join
+
+# Use this if you need low memory usage
+# but keep in mind, this is very slow
+# about 44h to read file with 800.000 lines
+#
+#f.each_line do |line|
+#  puts "adding #{line}"
+#  json_string += line
+#end
+#
 f.close
 
-
+puts "parsing json"
 json_data = JSON.parse(json_string, :max_nesting => 100)
+
 
 puts "deleting old database entries"
 MimeTab.delete_all
 KindTab.delete_all
 FileStructureEntity.delete_all
 
+
 # make mime tab
 puts "filling mime_tab"
 json_data["mime_tab"].each do |entry|
-  puts "processing #{entry["id"]} : #{entry["description"]}"
+  #puts "processing #{entry["id"]} : #{entry["description"]}"
   MimeTab.create( {:id => entry["id"], :description => entry["description"] })
 end
-
-puts ""
 
 # make kind tab
 puts "filling kind_tab"
 json_data["kind_tab"].each do |entry|
-  puts "processing #{entry["id"]} : #{entry["description"]}"
+  #puts "processing #{entry["id"]} : #{entry["description"]}"
   KindTab.create( {:id => entry["id"], :description => entry["description"] } )
 end
 
-puts ""
 
 puts "filling file_structure_entities"
 parse_fstruct(json_data["file_structure"])
